9a02ae387125b88eb5d733145f259a2ae5431abf
Firaol Berehanu <firaolberehanu@MacBook-Pro-79.local>
2024-05-23 21:51:11 -0700

    part 1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include "node.h"
+
+//Color codes
+#define RESET "\033[0m"
+#define RED   "\033[31m"
+#define BLUE  "\033[34m" //Substitute for black
+
+using namespace std;
+
