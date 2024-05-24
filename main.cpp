ce9d76216684bfb42690983bea67d21cf52e5920
Firaol Berehanu <firaolberehanu@MacBook-Pro-79.local>
2024-05-23 23:57:06 -0700

    part 2

--- main.cpp
+++ main.cpp
@@ -9,4 +9,36 @@
 #define BLUE  "\033[34m" //Substitute for black
 
 using namespace std;
+ 
+struct Branch { //Helper structure for printing
+    Branch* previous;
+    char* str;
+
+    Branch(Branch* previous, char* str) {
+        this->previous = previous;
+        this->str = str;
+    }
+};
+
+void showBranches(Branch* p) { //Helper function for printing
+    if (p == NULL)
+        return;
+
+    showBranches(p->previous);
+
+    cout << p->str;
+
+
+
+}
+
+
+void insertNode(Node*& root, Node*& current, Node*& parent, int value);
+void printTree(Node* root, Branch* previous, bool isLeft);
+void parseInput(char* input, int* values, int& count);
+void fileInsert(Node*& root);
+
+void balanceTree(Node*& root, Node*& current);
+void rotateLeft(Node*& root, Node*& current);
+void rotateRight(Node*& root, Node*& current);
 
